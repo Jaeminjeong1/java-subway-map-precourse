e619312bf19928e0c2e83bef597bb57457dc84ec
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-01-03 13:31:00 +0900

    feat(Validator): 각 스크린에서 입력 검증 로직 추가

--- src/main/java/subway/util/Validator.java
+++ src/main/java/subway/util/Validator.java
@@ -1,12 +1,15 @@
 package subway.util;
 
-import java.util.regex.Pattern;
+import java.util.List;
 
 import static subway.util.ErrorMessage.INPUT_ERROR;
 
 public class Validator {
 
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("^\\d+$");
+    private static final List<String> mainNumber = List.of("1", "2", "3", "4", "Q");
+    private static final List<String> stationNumber = List.of("1", "2", "3", "B");
+    private static final List<String> lineNumber = List.of("1", "2", "3", "B");
+    private static final List<String> sectionNumber = List.of("1", "2", "B");
     private static final int MIN_NUM = 1; // 수정
     private static final int MAX_NUM = 10; // 수정
 
@@ -16,6 +19,30 @@ public class Validator {
     // 빈값 검증
     public static void validateEmptyInput(String input) {
         if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(INPUT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateMainInput(String input) {
+        if (!mainNumber.contains(input)) {
+            throw new IllegalArgumentException(INPUT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateStationInput(String input) {
+        if (!stationNumber.contains(input)) {
+            throw new IllegalArgumentException(INPUT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateLineInput(String input) {
+        if (!lineNumber.contains(input)) {
+            throw new IllegalArgumentException(INPUT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateSectionInput(String input) {
+        if (!sectionNumber.contains(input)) {
             throw new IllegalArgumentException(INPUT_ERROR.getMessage());
         }
     }
